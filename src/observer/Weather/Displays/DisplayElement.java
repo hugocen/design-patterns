64f9144bef153b785a57691e45dfaeafb108fdde
Hugo Chen <hugo.chen731@gmail.com>
2023-04-16 17:16:52 +0800

    the observer pattern

---
+++ src/observer/Weather/Displays/DisplayElement.java
@@ -0,0 +1,5 @@
+package observer.Weather.Displays;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ src/observer/Weather/WeatherData/Subject.java
@@ -0,0 +1,9 @@
+package observer.Weather.WeatherData;
+
+import observer.Weather.Displays.Observer;
+
+public interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+}
