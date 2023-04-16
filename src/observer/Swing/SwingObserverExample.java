3ad71ddeeb97320acb5f51194e0f65e30d7c03f1
Hugo Chen <hugo.chen731@gmail.com>
2023-04-16 17:25:03 +0800

    observer swing example with lambda

--- src/observer/Swing/SwingObserverExample.java
+++ src/observer/Swing/SwingObserverExample.java
@@ -15,8 +15,8 @@ public class SwingObserverExample {
         frame = new JFrame();
 
         JButton button = new JButton("Should I do it?");
-        button.addActionListener(new AngelListener());
-        button.addActionListener(new DevilListener());
+        button.addActionListener(event -> System.out.println("Don't do it, you might regret it!"));
+        button.addActionListener(event -> System.out.println("Come on, do it!"));
 
         // Set frame properties here
         frame.getContentPane().add(BorderLayout.CENTER, button);
@@ -26,18 +26,6 @@ public class SwingObserverExample {
 		frame.getContentPane().add(BorderLayout.CENTER, button);
 		frame.setSize(300,300);
 		frame.setVisible(true);
-    }
-
-    class AngelListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("Don't do it, you might regret it!");
-        }
-    }
-
-    class DevilListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            System.out.println("Come on, do it!");
-        }
     }
 
 }
